cdbf4c9195c0f5cc3c0b0bbca07f5f1c5ce1492e
causon-mikolorenz <causonmikolorenz@gmail.com>
2025-06-11 13:41:00 +0800

    add constants

---
+++ iskolutions/causon.py
@@ -0,0 +1,9 @@
+DEFAULT_DICE = [1, 2, 3, 4, 5, 6]
+DEFAULT_NUMBER_OF_DICE = 1
+DEFAULT_HISTORY = []
+DEFAULT_STATISTICS = {
+    "Total Rolls": 0,
+    "Total Score": 0
+}
+UNSET_OPTION = -1
+EXIT_OPTION = 8
