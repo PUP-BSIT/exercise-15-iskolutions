bda4ad14de842d93b2d3b6b3d63ef0a412f00557
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-06-11 19:48:13 +0800

    add constructor

--- iskolutions/lopez.py
+++ iskolutions/lopez.py
@@ -1,4 +1,67 @@
 from os import system
 
 class TypeMemorizePup:
-    pass
+    def __init__(self):
+        self.content = {
+            "vision": "A Leading Comprehensive Polytechnic University "
+            + "in Asia",
+            "mission": "Advance an Inclusive, Equitable, and Globally "
+            + "Relevant Polytechnic Education Towards National "
+            + "Development",
+            "core_values": {
+                "i1": "Integrity and Accountability",
+                "n": "Nationalism",
+                "s": "Sense of Service",
+                "p": "Passion for Learning and Innovation",
+                "i2": "Inclusivity",
+                "r": "Respect for Human Rights and the Environment",
+                "e": "Excellence",
+                "d": "Democracy"
+            },
+            "pillars": {
+                "pillar1": {
+                    "name": "Teaching and Learning",
+                    "strategic_goals": {
+                        "strategic_goal1": "Innovative Curricula and "
+                        + "Instruction",
+                        "strategic_goal2": "Empowered Expert, and "
+                        + "Productive "
+                        + "Faculty Members",
+                        "strategic_goal3": "Holistic Student "
+                        + "Development"
+                    }
+                },
+                "pillar2": {
+                    "name": "Research and Extension",
+                    "strategic_goals": {
+                        "strategic_goal4": "Intensified Research "
+                        + "Innovation, "
+                        + "Dissemination and Utilization",
+                        "strategic_goal5": "Strengthened Sustainable "
+                        + "and Impactful "
+                        + "Extension Program",
+                        "strategic_goal6": "Expanded Research and "
+                        + "Extension "
+                        + "Networks with Local, National, and "
+                        + "International Partners"
+                    }
+                },
+                "pillar3": {
+                    "name": "Internal Governance",
+                    "strategic_goals": {
+                        "strategic_goal7": "Transformational "
+                        + "University Leadership",
+                        "strategic_goal8": "Judicious and Ethical "
+                        + "Stewardship of Physical and Financial "
+                        + "Resources",
+                        "strategic_goal9": "Effective and Efficient "
+                        + "Human Resources Management",
+                        "strategic_goal10": "Excellent Citizen/Client "
+                        + "Satisfaction",
+                        "strategic_goal11": "Smart Campuses"
+                    }
+                }
+            }
+        }
+        self.score = 0
+        self.attempt = 0
